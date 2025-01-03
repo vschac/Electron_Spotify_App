3204a8bed3a2255bba82cb3a05280fc45863889a
vschac <136936124+vschac@users.noreply.github.com>
2025-01-03 15:00:14 -0500

    Update main.js

--- src/main.js
+++ src/main.js
@@ -67,7 +67,7 @@ async function handleLogin() {
     querystring.stringify({
       response_type: 'code',
       client_id: client_id,
-      scope: 'user-read-private user-read-email user-library-read user-read-playback-state playlist-modify-public playlist-modify-private',
+      scope: 'user-read-private user-read-email user-library-read user-read-playback-state playlist-modify-public',
       redirect_uri: redirect_uri,
       show_dialog: true,
       state: Math.random().toString(36).substring(7)
